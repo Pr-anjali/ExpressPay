a5659c25b2e885cfae5e21452f1940094abe5481
Shatakshi <shatakshisrivastava354@gmail.com>
2023-06-15 00:58:27 -0700

    profile

--- client/src/components/Payment.js
+++ client/src/components/Payment.js
@@ -81,6 +81,69 @@ const PaymentPage = () => {
 
 
 
+  // return (
+  //   <div className="container-fluid payment-container">
+  //     <section>
+  //       <h2>Recharge</h2>
+  //       <div className="cards-container">
+  //         <div className="card">
+  //           <img src={mobileRechargeImage} alt="Mobile Recharge" />
+  //           <h3>Mobile Recharge</h3>
+  //         </div>
+  //         <div className="card">
+  //           <img src={dthRechargeImage} alt="DTH Recharge" />
+  //           <h3>DTH Recharge</h3>
+  //         </div>
+  //         <div className="card">
+  //           <img src={ottImage} alt="OTT" />
+  //           <h3>OTT</h3>
+  //         </div>
+  //         <div className="card">
+  //           <img src={cableTvImage} alt="Cable TV" />
+  //           <h3>Cable TV</h3>
+  //         </div>
+  //       </div>
+  //     </section>
+  //     <section>
+  //       <h2>Home Bills</h2>
+  //       <div className="cards-container">
+  //         <div className="card">
+  //           <img src={electricityImage} alt="Electricity" />
+  //           <h3>Electricity</h3>
+  //         </div>
+  //         <div className="card">
+  //           <img src={rentImage} alt="Rent" />
+  //           <h3>Rent</h3>
+  //         </div>
+  //         <div className="card">
+  //           <img src={educationFeesImage} alt="Education Fees" />
+  //           <h3>Education Fees</h3>
+  //         </div>
+  //       </div>
+  //     </section>
+  //     <section>
+  //       <h2>Subscription</h2>
+  //       <div className="cards-container">
+  //         <div className="card">
+  //           <img src={disneyHotstarImage} alt="Disney+ Hotstar" />
+  //           <h3>Disney+ Hotstar</h3>
+  //         </div>
+  //         <div className="card">
+  //           <img src={amazonPrimeImage} alt="Amazon Prime" />
+  //           <h3>Amazon Prime</h3>
+  //         </div>
+  //         <div className="card">
+  //           <img src={netflixImage} alt="Netflix" />
+  //           <h3>Netflix</h3>
+  //         </div>
+  //         <div className="card">
+  //           <img src={spotifyImage} alt="Spotify" />
+  //           <h3>Spotify</h3>
+  //         </div>
+  //       </div>
+  //     </section>
+  //   </div>
+  // );
   return (
     <div className="container-fluid payment-container">
       <section>
@@ -89,18 +152,22 @@ const PaymentPage = () => {
           <div className="card">
             <img src={mobileRechargeImage} alt="Mobile Recharge" />
             <h3>Mobile Recharge</h3>
+            <button className="card-button">Recharge Now</button>
           </div>
           <div className="card">
             <img src={dthRechargeImage} alt="DTH Recharge" />
             <h3>DTH Recharge</h3>
+            <button className="card-button">Recharge Now</button>
           </div>
           <div className="card">
             <img src={ottImage} alt="OTT" />
             <h3>OTT</h3>
+            <button className="card-button">Subscribe Now</button>
           </div>
           <div className="card">
             <img src={cableTvImage} alt="Cable TV" />
             <h3>Cable TV</h3>
+            <button className="card-button">Subscribe Now</button>
           </div>
         </div>
       </section>
@@ -110,14 +177,17 @@ const PaymentPage = () => {
           <div className="card">
             <img src={electricityImage} alt="Electricity" />
             <h3>Electricity</h3>
+            <button className="card-button">Pay Now</button>
           </div>
           <div className="card">
             <img src={rentImage} alt="Rent" />
             <h3>Rent</h3>
+            <button className="card-button">Pay Now</button>
           </div>
           <div className="card">
             <img src={educationFeesImage} alt="Education Fees" />
             <h3>Education Fees</h3>
+            <button className="card-button">Pay Now</button>
           </div>
         </div>
       </section>
@@ -127,23 +197,28 @@ const PaymentPage = () => {
           <div className="card">
             <img src={disneyHotstarImage} alt="Disney+ Hotstar" />
             <h3>Disney+ Hotstar</h3>
+            <button className="card-button">Subscribe Now</button>
           </div>
           <div className="card">
             <img src={amazonPrimeImage} alt="Amazon Prime" />
             <h3>Amazon Prime</h3>
+            <button className="card-button">Subscribe Now</button>
           </div>
           <div className="card">
             <img src={netflixImage} alt="Netflix" />
             <h3>Netflix</h3>
+            <button className="card-button">Subscribe Now</button>
           </div>
           <div className="card">
             <img src={spotifyImage} alt="Spotify" />
             <h3>Spotify</h3>
+            <button className="card-button">Subscribe Now</button>
           </div>
         </div>
       </section>
-    </div>
+    </div> 
   );
+  
 };
 
 export default PaymentPage;
